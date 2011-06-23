c31d210508395e30f54b96425dd8cea45d1b8d04
Nathan Friedly <nathan@nfriedly.com>
2011-06-23 14:40:02 -0400

    2011-06-23

--- update_fb_github.sh
+++ update_fb_github.sh
@@ -1,7 +1,6 @@
-pushd /home/nfriedly/facebook/connect-js/
+cd /home/nfriedly/facebook/connect-js/
 /usr/bin/wget https://connect.facebook.net/en_US/all.js -O all.js
 /usr/bin/python jsbeautifier.py -o all_deminified.js all.js
 /usr/bin/git add .
 /usr/bin/git commit -m `date --rfc-3339=date`
 /usr/bin/git push origin master
-popd
