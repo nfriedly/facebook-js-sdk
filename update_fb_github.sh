fbe0eed8e1aefede64fc0b2a5ac1bd655d94b8c4
Nathan Friedly <nathan@nfriedly.com>
2011-06-28 14:45:37 -0400

    Now diffs the latest all.js with the previous to see if anything besides the timestamp has changed, and only saves and commits changes if so.

--- update_fb_github.sh
+++ update_fb_github.sh
@@ -8,17 +8,33 @@
 
 # change  to the directory where this script is located
 cd `dirname $0`
-#download the latest facebook all.js
+
+# make a backup copy to diff against the latest all.js. 
+mv all.js all_old.js
+
+# download the latest facebook all.js
 /usr/bin/wget https://connect.facebook.net/en_US/all.js -O all.js
 
-# use sed to remove the timestamp from the beginning
-# (avoids usless commits where nothing but the timestamp changed)
-/bin/sed -i 's/\/\*[0-9]\+,[0-9]\+,JIT/\/*\[timestamp removed\],JIT/' all.js
+# compare the latest with the backup to see if anything besides the
+# timestamp comment at the top changed
+DIFF=$(/usr/bin/diff --brief --ignore-matching-lines=\/\*.*\*\/  all.js all_old.js)
 
-# deminify the script
-/usr/bin/python jsbeautifier.py -o all_deminified.js all.js
+# if anything has changed besides the timestamp
+if [[ $DIFF != "" ]]; then
+	
+	# get rid of the old file
+	rm all_old.js
+	
+	# deminify the script
+	/usr/bin/python jsbeautifier.py -o all_deminified.js all.js
 
-# and, lastly, send it to github
-/usr/bin/git add *.js
-/usr/bin/git commit -m `date --rfc-3339=date`
-/usr/bin/git push origin master
+	# and, lastly, send it to github
+	/usr/bin/git add *.js
+	/usr/bin/git commit -m `date --rfc-3339=date`
+	/usr/bin/git push origin master
+else 
+	
+	# put the old file back (keeps the timestamps in sync between
+	# minified and deminified)
+	mv all_old.js all.js
+fi
