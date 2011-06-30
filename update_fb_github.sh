27e65c070126bb0ac121aba22d2326e25a6d4678
Nathan Friedly <nathan@nfriedly.com>
2011-06-30 11:37:45 -0400

    refactored shell script to use a function for the repetitive part, adds css/js seperately, only sends actuall errors to stderr, and one line to stdout

--- update_fb_github.sh
+++ update_fb_github.sh
@@ -8,48 +8,79 @@
 # This script is released under an MIT License. Accompanying files 
 # may have different licenses.
 
+
+###
+# Function that downloads the latest all.js/css file and compares it to the old one.
+# 
+# If only the timestamp has changed, it restores the old file
+#
+# @param {string} fileType - "js" or "css"
+# @return {int} changed - 0 if nothing but the timestamp changed, 1 if other lines have changed
+###
+getChanges(){
+	# make a backup copy to diff against the latest all.js/css.
+	mv all.$1 all_old.$1
+
+	# download the latest facebook all.js/css
+	/usr/bin/wget -q  -O all.$1 https://connect.facebook.net/en_US/all.$1
+
+	# compare the latest with the backup to see if anything besides the
+	# timestamp comment at the top changed
+	local changes=$(/usr/bin/diff --brief --ignore-matching-lines=\/\*.*\*\/  all.$1 all_old.$1)
+	
+	if [[ changes == "" ]]; then 
+		# no changes, or only the timestamp changed
+		# put back the old file
+		mv all_old.$1 all.$1
+		return 0
+	else
+		# something else has changed
+		# get rid of the old file
+		rm all_old.$1
+		return 1
+	fi
+}
+
 # change  to the directory where this script is located
 cd `dirname $0`
 
-# make a backup copy to diff against the latest all.js/css. 
-mv all.js all_old.js
-mv all.css all_old.css
 
-# download the latest facebook all.js/css
-/usr/bin/wget https://connect.facebook.net/en_US/all.js -O all.js
-/usr/bin/wget https://connect.facebook.net/en_US/all.css -O all.css
+JS_CHANGES=$(getChanges "js")
 
-# compare the latest with the backup to see if anything besides the
-# timestamp comment at the top changed
-JS_CHANGES=$(/usr/bin/diff --brief --ignore-matching-lines=\/\*.*\*\/  all.js all_old.js)
-CSS_CHANGES=$(/usr/bin/diff --brief --ignore-matching-lines=\/\*.*\*\/  all.css all_old.css)
+CSS_CHANGES=$(getChanges "css")
 
-# if anything has changed besides the timestamp
-if [[ $JS_CHANGES != "" || $CSS_CHANGES != "" ]]
-then
-	
-	# get rid of the old file
-	rm all_old.js
-	rm all_old.css
+# if anything has changed besides the timestamp in the JS
+if [[ $JS_CHANGES ]]; then
 	
 	# deminify the script
 	/usr/bin/python lib/jsbeautifier.py -o all_deminified.js all.js
 	
+	# add it to the staging area
+	/usr/bin/git add *.js
+fi
+
+# if anything has changed besides the timestamp in the CSS
+if [[ $CSS_CHANGES ]]; then
+	
+	# deminify the file	
 	# this requires php5-cli
 	/usr/bin/php lib/csstidy_wrapper.php all.css all_deminified.css
+	
+	# add it to the staging area
+	/usr/bin/git add *.css
+fi
 
-	# and, lastly, send it to github
-	/usr/bin/git add *.js
-	/usr/bin/git add *.css	
+# if anything changed in either file, commit and push to github
+if [[ $JS_CHANGES || $CSS_CHANGES ]]; then
+
+	echo "Sending changes to github"
 	
 	# commit with yesterday's date since this is typically run
 	# shortly after midnight
 	/usr/bin/git commit -m "FB changes from `date --date=yesterday +'%A, %B %-d, %Y'`"
-	/usr/bin/git push origin master
-else 
+	/usr/bin/git push -q origin master
 	
-	# put the old file back (keeps the timestamps in sync between
-	# minified and deminified)
-	mv all_old.js all.js
-	mv all_old.css all.css
+else
+	echo "No changes"
 fi
+
