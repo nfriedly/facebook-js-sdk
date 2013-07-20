ddefad2e5fe59914a5acf18ff9ed50d1ec089315
Nathan Friedly <nathan@nfriedly.com>
2013-07-20 10:48:43 -0400

    changing repo name

--- heroku.sh
+++ heroku.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-# ./tmp is the only writeable directory on heroku instances. It is regularly wiped.
+# ./tmp is the only writeable directory on heroku instances, and it is regularly wiped.
 # so, we check if our file is already there, and if not, clone the repo to ./tmp
 if [ ! -f tmp/debug.js ]; then
-  git clone https://github.com/nfriedly/connect-js tmp
+  # you must set the $GH_USER & $GH_PASS variables with `heroku config:add GH_USER=username`
+  git clone https://$GH_USER:$GH_PASS@github.com/nfriedly/facebook-js-sdk tmp
 fi
 
 # lastly, run our script
